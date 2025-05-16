e3a72cc55e05ea95876cadcd9dcf94254a421f04
NATSIIRT <tristan.baud@proton.me>
2025-05-16 14:19:13 +0200

    add routes

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,9 @@
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>Welcome to the Sign In page!</p>
+    </div>
+  );
+}
+export default SignIn;
---
+++ src/pages/Vault.tsx
@@ -0,0 +1,9 @@
+function Vault() {
+  return (
+    <div>
+      <h1>Vault</h1>
+      <p>This is the Vault page.</p>
+    </div>
+  );
+}
+export default Vault;
